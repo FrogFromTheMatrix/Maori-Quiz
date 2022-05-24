f21f4276e85b5324e6cab8cb8440362c61543858
Riley Sullivan <101753502+FrogFromTheMatrix@users.noreply.github.com>
2022-05-24 01:49:51 +0000

    Started the questions part

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-name = input("hello, what is your name? ")
+n = input("Hello, what is your name? ")
 
 x = input(
-  f'Kia Ora {name}, Would you like to play a te reo quiz to test your Te reo'
-"Capabilities")
+  f'Kia Ora {n} Would you like to play a Te reo quiz? ').lower()
 
 
 
 
 if x == "yes" or x == "y":
-        print(
-            "You can buy a pie for $4.50 or a burger for $7.89, What would you like to buy? ")
-  
-elif x == "n" or x == "no":
-        exit("Goodbye")
-
-else:
-      print("Please answer Yes or No")
+         
