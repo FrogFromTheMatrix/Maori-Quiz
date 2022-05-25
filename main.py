838d4d16f175f02f17ff7a278fd90fb54bdc6ff0
Riley Sullivan <101753502+FrogFromTheMatrix@users.noreply.github.com>
2022-05-25 03:08:22 +0000

    Made the first question, still fixing bugs

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 n = input("Hello, what is your name? ")
 
 x = input(
-  f'Kia Ora {n} Would you like to play a Te reo quiz? ').lower()
+  f'Kia Ora {n} Would you like to play a maori language quiz? ').lower()
 
 
 
 
 if x == "yes" or x == "y":
+ l = input("What is the maori word for coding? ").lower()
+if l == "whakawaehere":
+ print("Correct!")
+
+else:
+  exit("Fail!")
          
