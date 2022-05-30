9617a3fadfd0ed584cc0e25ef87ded1974b2711f
Riley Sullivan <101753502+FrogFromTheMatrix@users.noreply.github.com>
2022-05-30 01:00:34 +0000

    Added 3 more questions, starting researching how to add a lives system

--- main.py
+++ main.py
@@ -10,7 +10,29 @@ if x == "yes" or x == "y":
  q1 = input("What is the maori word for Coding? ").lower()
 if q1 == "whakawaehere":
  print("Correct!")
-
-q2 = input("What is the maori word for Hardware").lower()
-if q2 == "Pumaro ":
+else:
+  exit("Incorrect")
+  
+q2 = input("What is the maori word for Hard Drive? ").lower()
+if q2 == "pumaro":
   print("Correct!")
+else:
+  exit("Incorrect")
+  
+q3 = input("What is the maori word for input?  ").lower()
+if q3 == "tauru":
+ print("Correct!")
+else:
+  exit("Incorrect")
+  
+q4 = input("What is the maori word for output? ").lower()
+if q4 == "pungao puta":
+   print("Correct")
+else:
+  exit("Incorrect")
+  
+q5 = input("what is the maori word for programming? ").lower()
+if q5 == "putirotiro":
+  print("Correct")
+else:
+  exit("Incorrect")
