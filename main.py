3e7bbb05f325eba29c9b66e0a6d537a905a64eb1
Riley Sullivan <101753502+FrogFromTheMatrix@users.noreply.github.com>
2022-05-31 01:50:29 +0000

    Started lives system and a define loop, made question 5 and fixed lives bug.

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
+counter = 3
+
 n = input("Hello, what is your name? ")
 
 x = input(
   f'Kia Ora {n} Would you like to play a maori language quiz? ').lower()
+def counter():
+ if counter = 0
+ print("You ran out of lives, Game Over!!!")
 
-
-
+counter()
 
 if x == "yes" or x == "y":
  q1 = input("What is the maori word for Coding? ").lower()
 if q1 == "whakawaehere":
- print("Correct!")
+ print(f'Correct! you have still have {counter} lives left!' )
 else:
-  exit("Incorrect")
+  print(f'Incorrect, You now have {counter} lives left!')
+  counter -= 1
+  
   
 q2 = input("What is the maori word for Hard Drive? ").lower()
 if q2 == "pumaro":
-  print("Correct!")
+  print(f'Correct! you have still have {counter} lives left!' )
 else:
-  exit("Incorrect")
-  
+  print(f'Incorrect, You now have {counter} lives left!')
+counter -= 1
+
 q3 = input("What is the maori word for input?  ").lower()
 if q3 == "tauru":
- print("Correct!")
+ print(f'Correct! you have still have {counter} lives left!' )
 else:
-  exit("Incorrect")
-  
+  print(f'Incorrect, You now have {counter} lives left!')
+  counter -= 1
+
 q4 = input("What is the maori word for output? ").lower()
 if q4 == "pungao puta":
-   print("Correct")
+   print(f'Correct! you have still have {counter} lives left!' )
 else:
-  exit("Incorrect")
-  
+  print(f'Incorrect, You now have {counter} lives left!')
+counter -= 1
+
 q5 = input("what is the maori word for programming? ").lower()
 if q5 == "putirotiro":
-  print("Correct")
+  print(f'Correct! you have still have {counter} lives left!' )
 else:
-  exit("Incorrect")
+  print(f'Incorrect, You now have {counter} lives left!')
+  counter -= 1
+  
+  q6 = input("what is the maori word for print? ").lower()
+if q6 == "matatuhi":
+  print(f'Correct! you have still have {counter} lives left!' )
+else:
+  print(f'Incorrect, You now have {counter} lives left!')
+counter -= 1
