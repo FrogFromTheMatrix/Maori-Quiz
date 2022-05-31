45006d313f919e6594fa4ebc5cef25fa17d387f7
Riley Sullivan <101753502+FrogFromTheMatrix@users.noreply.github.com>
2022-05-31 08:56:47 +0000

    Made more progress on lives counter but have to undo because of errors with my type of code

--- main.py
+++ main.py
@@ -4,52 +4,69 @@ n = input("Hello, what is your name? ")
 
 x = input(
   f'Kia Ora {n} Would you like to play a maori language quiz? ').lower()
-def counter():
- if counter = 0
- print("You ran out of lives, Game Over!!!")
 
-counter()
+
+
 
 if x == "yes" or x == "y":
  q1 = input("What is the maori word for Coding? ").lower()
-if q1 == "whakawaehere":
- print(f'Correct! you have still have {counter} lives left!' )
-else:
-  print(f'Incorrect, You now have {counter} lives left!')
-  counter -= 1
-  
-  
-q2 = input("What is the maori word for Hard Drive? ").lower()
-if q2 == "pumaro":
+ if q1 == "whakawaehere":
   print(f'Correct! you have still have {counter} lives left!' )
-else:
-  print(f'Incorrect, You now have {counter} lives left!')
-counter -= 1
-
-q3 = input("What is the maori word for input?  ").lower()
-if q3 == "tauru":
- print(f'Correct! you have still have {counter} lives left!' )
-else:
+ else:
   print(f'Incorrect, You now have {counter} lives left!')
   counter -= 1
+elif counter == 0:
+  print("you ran out of lives, game over")
 
-q4 = input("What is the maori word for output? ").lower()
-if q4 == "pungao puta":
+
+  q2 = input("What is the maori word for Hard Drive? ").lower()
+  if q2 == "pumaro":
+    print(f'Correct! you have still have {counter} lives left!' )
+  else:
+    print(f'Incorrect, You now have {counter} lives left!')
+  counter -= 1
+elif counter == 0:
+  print("you ran out of lives, game over")
+
+  
+  q3 = input("What is the maori word for input?  ").lower()
+  if q3 == "tauru":
    print(f'Correct! you have still have {counter} lives left!' )
-else:
-  print(f'Incorrect, You now have {counter} lives left!')
-counter -= 1
+  else:
+    print(f'Incorrect, You now have {counter} lives left!')
+    counter -= 1
+elif counter == 0:
+  print("you ran out of lives, game over")
 
-q5 = input("what is the maori word for programming? ").lower()
-if q5 == "putirotiro":
-  print(f'Correct! you have still have {counter} lives left!' )
-else:
-  print(f'Incorrect, You now have {counter} lives left!')
-  counter -= 1
+
   
+  q4 = input("What is the maori word for output? ").lower()
+  if q4 == "pungao puta":
+     print(f'Correct! you have still have {counter} lives left!' )
+  else:
+    print(f'Incorrect, You now have {counter} lives left!')
+  counter -= 1
+elif counter == 0:
+  print("you ran out of lives, game over")
+
+
+  
+  q5 = input("what is the maori word for programming? ").lower()
+  if q5 == "putirotiro":
+    print(f'Correct! you have still have {counter} lives left!' )
+  else:
+    print(f'Incorrect, You now have {counter} lives left!')
+    counter -= 1
+elif counter == 0:
+  print("you ran out of lives, game over")
+
+
+    
   q6 = input("what is the maori word for print? ").lower()
-if q6 == "matatuhi":
-  print(f'Correct! you have still have {counter} lives left!' )
-else:
-  print(f'Incorrect, You now have {counter} lives left!')
-counter -= 1
+  if q6 == "matatuhi":
+    print(f'Correct! you have still have {counter} lives left!' )
+  else:
+    print(f'Incorrect, You now have {counter} lives left!')
+    counter -= 1
+elif counter == 0:
+  print("you ran out of lives, game over")
